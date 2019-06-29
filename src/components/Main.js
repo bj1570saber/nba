77e925b7f3b862c6bd483e748710f9d608767c65
bj1570Saber <bj1570saber@gmail.com>
2019-06-29 13:05:18 -0700

    modified: Main.js Profile.js Profile.css; added constants.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import {ShotChart} from './ShotChart';
 import {Profile} from './Profile'
+import nba from 'nba';
+
 export class Main extends React.Component{
+    state = {
+        playerId: 201939,
+        playerInfo: {},
+    }
+
+    componentDidMount(){
+        nba.stats.playerInfo({PlayerID: this.state.playerId})
+            .then((info) =>{
+                const playerInfo = Object.assign({}, 
+                    info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log("final player info", playerInfo);
+                this.setState({playerInfo});
+            })
+            .catch((e) =>{
+                console.log(e);
+            });
+    }
+
     render(){
         return(
             <div className = "main">
-                <Profile />
+                <Profile playerInfo = {this.state.playerInfo}/>
                 <ShotChart playerId = {201939}/>
             </div>
         );
